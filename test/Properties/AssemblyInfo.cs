3f23d3feeafdf1d542ecbbf97565378a7b0a43ce
Rosana <rodjuga@gmail.com>
2018-12-24 00:51:31 +0100

    App Tko je Dužan - calculate your hangouts

--- test/Properties/AssemblyInfo.cs
+++ test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpajanjeNaBazuTest")]
+[assembly: AssemblyTitle("test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SpajanjeNaBazuTest")]
+[assembly: AssemblyProduct("test")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("82137a87-5144-404b-b05f-c5e1de47457d")]
+[assembly: Guid("e12de555-5639-455e-a9ab-e873b6f334f1")]
 
 // Version information for an assembly consists of the following four values:
 //
